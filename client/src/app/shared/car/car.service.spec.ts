953576014e09d897308620e41d74e20a0e9f660a
Matt Raible <matt.raible@okta.com>
2018-04-04 16:40:40 -0600

    Fix Angular unit and e2e tests

--- client/src/app/shared/car/car.service.spec.ts
+++ client/src/app/shared/car/car.service.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { CarService } from './car.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CarService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule],
       providers: [CarService]
     });
   });
